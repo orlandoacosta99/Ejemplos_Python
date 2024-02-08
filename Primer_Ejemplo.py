5c1d582bdaa3802a7e5383ec999df93d557fe65c
orlandoacosta99 <36582050+orlandoacosta99@users.noreply.github.com>
2024-02-08 19:23:17 +0000

    ejemplo de funcion

--- Primer_Ejemplo.py
+++ Primer_Ejemplo.py
@@ -1,1 +1,5 @@
-print("Ejemplo en terminal")
+def perimetro_cuadrado(lado, unidades):
+        perimetro =  lado*4
+        print(f"El perimetro es {perimetro} {unidades}" )
+
+perimetro_cuadrado(25,"Metros")
